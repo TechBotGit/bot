6962117e4901e1d03403abdbfb10122ccc0e2e8d
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-09-06 01:26:16 +0800

    echo the message!

---
+++ samples/echoMessage.py
@@ -0,0 +1,22 @@
+import sys
+import time
+import telepot
+from telepot.loop import MessageLoop
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, msg['text'])
+
+token = "411692236:AAFT7dtWSr_FM56juxyneZmoqPZD_qqxhR0"
+bot = telepot.Bot(token)
+
+MessageLoop(bot, handle).run_as_thread()
+print("Listening...")
+
+# Keep the program running.
+while 1:
+    time.sleep(10)

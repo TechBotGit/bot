d8181c988068a8cedce8aae08cfdea3fc0aa729e
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-09-07 03:20:12 +0800

    successfully create event!

---
+++ samples/createEvent_gcal.py
@@ -0,0 +1,97 @@
+# Refer to the Python quickstart on how to setup the environment:
+# https://developers.google.com/google-apps/calendar/quickstart/python
+# Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+# stored credentials.
+
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = '../resources/api/client_secret.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else:  # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def createEvent():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    # Event Details
+    event = {
+        'summary': 'Meeting Bot',
+        'location': 'Nanyang Technological University',
+        'description': 'Let us be a TechBot',
+        'start': {
+            'dateTime': '2017-09-8T10:00:00',
+            'timeZone': 'Asia/Singapore',
+        },
+        'end': {
+            'dateTime': '2017-09-8T14:00:00',
+            'timeZone': 'Asia/Singapore',
+        },
+        'attendees': [
+            {'email': 'jasoncobalagi@gmail.com'}
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 60}
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
+
+if __name__ == '__createEvent__':
+    createEvent()

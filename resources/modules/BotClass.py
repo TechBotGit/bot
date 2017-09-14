cad63d3829c98ef7bf0109b8e8b78178ead18e02
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-09-14 10:56:30 +0800

    Create Event v1

--- resources/modules/BotClass.py
+++ resources/modules/BotClass.py
@@ -1,7 +1,8 @@
-import sys
 import os
 import telepot
 from telepot.loop import MessageLoop
+import sys
+import GoogleapiClass as gc
 
 
 class API(object):
@@ -13,34 +14,59 @@ class API(object):
         self.token = f.read()
         f.close()
         self.bot = telepot.Bot(self.token)
+        self.update_id = None
 
     def handleAPI(self, msg):
         content_type, chat_type, chat_id = telepot.glance(msg)
         print(content_type, chat_type, chat_id)  # debug msg received
+        response = self.bot.getUpdates()
 
         if content_type == 'text':
-           
+
             # Convert the message to lower case
             msg_received = msg['text'].lower()
-            
+            print(msg_received)
+
             # If the message is a valid command
             if BotCommand().isValidCommand(msg_received):
                 if msg_received == '/start':
-                    self.bot.sendMessage(chat_id, "Beep. You can start chatting with me now, or ask me to do stuff. :)")
+                    msg_reply = "Beep. You can start chatting with me now, or ask me to do stuff. :)"
+                    self.bot.sendMessage(chat_id, msg_reply)
 
                 elif msg_received == '/createevent':
-                    self.bot.sendMessage(chat_id, "Okay send me the details in the format EventName, dd/mm/yy:hh:mm - dd/mm/yy/:hh:mm")
+                    msg_reply = "Okay send me the details in following format: \n"
+                    str_format = "Event Name;location;yyyy-mm-ddThh:mm;yyyy-mm-ddThh:mm"
+                    self.bot.sendMessage(chat_id, msg_reply)
+                    self.bot.sendMessage(chat_id, str_format)
+                    print(response)
 
                 elif msg_received == '/quit':
                     self.bot.sendMessage(chat_id, "Bye :(")
-        
+    
                 else:
                     self.bot.sendMessage(chat_id, "Command not updated!")
 
             else:
 
+                if msg['text'] == 'Telegram Event;Nanyang Technological University;2017-09-20T14:00:00;2017-09-20T16:00:00':
+                    str_input = StringParse(msg['text'])
+                    str_input.Parse()
+                    event_name = str_input.event_name
+                    location = str_input.location
+                    start_date = str_input.start_date
+                    end_date = str_input.end_date
+                    
+                    try:
+                        gc.GoogleAPI().createEvent(event_name, location, start_date, end_date)
+                    
+                    except:
+                        self.bot.sendMessage(chat_id, 'Cannot create event!')
+                    
+                    else:
+                        self.bot.sendMessage(chat_id, 'Successful!!')
+                
                 # If the bot knows reply the message
-                if BotReply().isValidtoReply(msg_received):
+                elif BotReply().isValidtoReply(msg_received):
                     print(BotReply().reply_dict[msg_received])
 
                     # With name?
@@ -98,3 +124,35 @@ class BotCommand(API):
 
     def isValidCommand(self, command):
         return command in self.command_list
+
+
+class StringParse(object):
+    """This is a class for string formatting"""
+
+    def __init__(self, str_message):
+        self.str_message = str_message
+        self.event_name = ''
+        self.location = ''
+        self.start_date = ''
+        self.end_date = ''
+    
+    def Parse(self):
+        semicolon = []
+
+        for l in self.str_message:
+            if l != ';':
+                if len(semicolon) == 0:
+                    self.event_name += l
+                    
+                elif len(semicolon) == 1:
+                    self.location += l
+                    
+                elif len(semicolon) == 2:
+                    self.start_date += l
+                    
+                elif len(semicolon) == 3:
+                    self.end_date += l
+
+            else:
+                semicolon.append(';')
+                continue
---
+++ resources/modules/GoogleapiClass.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+import datetime
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+
+class GoogleAPI(object):
+
+    def __init__(self):
+        # If modifying these SCOPES, delete your previously saved credentials
+        # at ~/.credentials/calendar-python-quickstart.json
+        self.SCOPES = 'https://www.googleapis.com/auth/calendar'
+        self.CLIENT_SECRET_FILE = '../api/client_secret.json'
+        self.APPLICATION_NAME = 'Google Calendar API Python Quickstart'
+
+    def get_credentials(self):
+        home_dir = os.path.expanduser('~')
+        credential_dir = os.path.join(home_dir, '.credentials')
+        if not os.path.exists(credential_dir):
+            os.makedirs(credential_dir)
+        credential_path = os.path.join(credential_dir, 'calendar-python-quickstart.json')
+        store = Storage(credential_path)
+        credentials = store.get()
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.CLIENT_SECRET_FILE, self.SCOPES)
+            flow.user_agent = self.APPLICATION_NAME
+            if flags:
+                credentials = tools.run_flow(flow, store, flags)
+            else:  # Needed only for compatibility with Python 2.6
+                credentials = tools.run(flow, store)
+            print('Storing credentials to ' + credential_path)
+        return credentials
+
+    def createEvent(self, summary, location, start, end):
+        credentials = self.get_credentials()
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http)
+
+        # Event Details
+        event = {
+            'summary': summary,
+            'location': location,
+            'description': 'Let us be a TechBot',
+            'start': {
+                'dateTime': start,
+                'timeZone': 'Asia/Singapore',
+            },
+            'end': {
+                'dateTime': end,
+                'timeZone': 'Asia/Singapore',
+            },
+            'attendees': [
+                {'email': 'jasoncobalagi@gmail.com'}
+            ],
+            'reminders': {
+                'useDefault': False,
+                'overrides': [
+                    {'method': 'email', 'minutes': 60}
+                ],
+            },
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))

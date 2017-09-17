768ec8d98409593f3775d16f10f7e7557346c9bc
TechBotGit <31700016+TechBotGit@users.noreply.github.com>
2017-09-18 00:33:51 +0800

    simplify test.py

--- samples/test.py
+++ samples/test.py
@@ -4,10 +4,10 @@ import sys
 
 cwd = os.path.dirname(sys.argv[0])
 path_file = cwd + '/a.txt'
-
-with open(path_file, 'r') as f:
-	token = (f.read())
-
+f = open(path_file, "r")
+token = (f.read())
+f.close()
 bot = telepot.Bot(token)
 
 print(bot.getMe())
+

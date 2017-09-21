eff6acfb785ae7d18343a0765279c91a27ae6c73
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-09-22 03:50:15 +0800

    appending rows

--- resources/database/get_db.py
+++ resources/database/get_db.py
@@ -1,10 +1,27 @@
 import pandas as pd
+import os
+import sys
+from openpyxl import load_workbook, Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-file = "database.xlsx"
+cwd = os.path.dirname(sys.argv[0])
+os.chdir(cwd)
 
-load = pd.ExcelFile(file)
+filename = "database.xlsx"
 
-print(load.sheet_names)
+wb = load_workbook(filename)
+sheet = wb.active
 
-df1 = load.parse('Sheet1')
-print(df1.head())
+sheet['A1'] = 'user_id'
+sheet['B1'] = 'chat_id'
+sheet['C1'] = 'event_id'
+
+new_user = 848
+new_chat = 134
+new_event = 103748952384
+
+update_list = [new_user, new_chat, new_event]
+
+sheet.append(update_list)
+
+wb.save(filename)

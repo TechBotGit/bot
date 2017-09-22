c3861cd048ad5347903f34dff3ab3d9fa07c1958
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-09-22 23:02:29 +0800

    restructured DB

--- resources/modules/DBClass.py
+++ resources/modules/DBClass.py
@@ -17,6 +17,14 @@ class DB(object):
         self.sheet['A1'] = 'chat_id'
         self.sheet['B1'] = 'first_week'
         self.sheet['C1'] = 'first_recess_week'
+        
+        # If file doesn't exist, create it
+        if not os.path.isfile(self.path_file):
+            self.wb.save(self.path_file)
+        
+        # for updating method
+        self.wb_update = load_workbook(self.path_file)
+        self.sheet_update = self.wb_update.active
 
         self._chat_id_list = []
 
@@ -31,17 +39,15 @@ class DB(object):
     
     def update(self, chat_id, first_week, first_recess_week):
         """Update exisiting workbook"""
-        if not os.path.isfile(self.path_file):
-            self.wb.save(self.path_file)
-        wb_update = load_workbook(self.path_file)
-        sheet_update = wb_update.active
-
-        for i in range(1, len(sheet_update['A'])):
-            self.chat_id_list.append(sheet_update['A'][i].value)
-        if chat_id not in self.chat_id_list:
+        if not self.isChatidExist(chat_id):
             update_list = [chat_id, first_week, first_recess_week]
-            sheet_update.append(update_list)
-            wb_update.save(self.path_file)
+            self.sheet_update.append(update_list)
+            self.wb_update.save(self.path_file)
         else:
             print('chat_id already exists')
             raise ValueError
+
+    def isChatidExist(self, chat_id):
+        for i in range(1, len(self.sheet_update['A'])):
+            self.chat_id_list.append(self.sheet_update['A'][i].value)
+        return chat_id in self.chat_id_list
